1f820f168f3c994620a260875ca385601ed14990
Fabian Schuetz <fabian.m.schuetz@googlemail.com>
2015-05-20 18:12:25 +0200

    switched hid input to python evdev

--- game.py
+++ game.py
@@ -1,32 +1,35 @@
 from pygre import Scene
-from input import HIDInput
+from input import HIDInput, get_devices
 
 import sys
 import signal
 import os
 import collections
-import re
 import threading
-
-re_evdev = re.compile("event\d+")
-
 
 class Game(Scene):
     def __init__(self):
         self.__lock__ = threading.Lock()
+        self.downed_keys = {}
         Scene.__init__(self)
-        self.__events = []
-        for devname in [evdev for evdev in os.listdir('/dev/input') if re_evdev.match(evdev) is not None ]:
-            evdev = HIDInput("/dev/input/%s" % devname, self)            
+        self.__events = {}
+        self.input_providers = []
+        for devname in get_devices():
+            evdev = HIDInput(self, devname)            
+            self.input_providers.append(evdev)
             evdev.start()
         def handle_sigint(signum, frame):
+            for evdev in self.input_providers:
+                evdev.stop()                
             self.stop()
-            sys.exit()
         signal.signal(signal.SIGINT, handle_sigint)
 
     def dispatch(self, *args):
         with self.__lock__:
-            self.__events.append(args)
+            if args[2] == 0:
+                del self.__events[args[0]]
+            else:
+                self.__events[args[0]] = args
 
     @property
     def events(self):
@@ -39,4 +42,3 @@ class Game(Scene):
     def __update__(self, overruns):
         with self.__lock__:
             self.update(overruns)
-            del self.__events[:]
